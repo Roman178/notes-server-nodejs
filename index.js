cb3473c706c1144156282b9933a5f2025211a92c
Roman <ismagilov771@gmail.com>
2022-05-01 11:45:33 +0300

    fix id

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ app.post("/notes", (req, res) => {
   fs.readFile("./db.json", (err, data) => {
     try {
       const notes = JSON.parse(data).notes;
-      notes.push({ ...req.body, id: +new Date() });
+      const noteToCreate = { ...req.body, id: +new Date() };
+      notes.push(noteToCreate);
       fs.writeFileSync("./db.json", JSON.stringify({ notes: notes }));
-      res.status(200).json(req.body);
+      res.status(200).json(noteToCreate);
     } catch (error) {
       res.status(500).json({ message: "Internal server error", error: err });
     }
